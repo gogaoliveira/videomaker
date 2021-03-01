880f98216bd7c9331dbd8174c1446b9b8e0daab8
de Oliveira <douglas.deoliveira@thomsonreuters.com>
2021-03-01 16:05:06 -0300

    feat: add search term and prefix

---
+++ index.js
@@ -0,0 +1,25 @@
+const readline = require('readline-sync')
+
+function start(){
+	const content = {}
+
+	content.searchTerm = askAndReturnSearchTerm()
+	content.prefix = askAndReturnPrefix()
+
+	function askAndReturnSearchTerm(){
+		return readline.question('escolha o termo no Wikipedia: ')
+	}
+
+	function askAndReturnPrefix(){
+		const prefixes = ['Quem e', 'O que e', 'a Historia do']
+		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opcao')
+		const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+		return selectedPrefixText
+	}
+
+	console.log(content)
+
+
+}
+start()
